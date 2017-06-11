919ae6e985d4da88024de66767eec7ccc82b8fed
Jim Wheaton <jimwheaton@gmail.com>
2017-06-11 16:01:22 -0700

    Finish capybara

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,42 @@ class Task < ActiveRecord::Base
     complete? && completed_at >= Project.velocity_length_in_days.days.ago
   end
 
+  def first_in_project?
+    return false unless project
+    project.tasks.first == self
+  end
+
+  def last_in_project?
+    return false unless project
+    project.tasks.last == self
+  end
+
+  def my_place_in_tasks
+    project.tasks.index(self)
+  end
+
+  def move_up
+    swap_order_with(previous_task)
+  end
+
+  def move_down
+    swap_order_with(next_task)
+  end
+
+  def swap_order_with(other)
+    other.project_order, self.project_order = self.project_order, other.project_order
+    other.save
+    self.save
+  end
+
+  def next_task
+    project.tasks[my_place_in_tasks + 1]
+  end
+
+  def previous_task
+    project.tasks[my_place_in_tasks - 1]
+  end
+
   def points_toward_velocity
     part_of_velocity? ? size : 0
   end
